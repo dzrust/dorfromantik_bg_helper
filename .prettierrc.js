60b229dde5754cfc1ea1ddc5d5fbae455eb7ca1a
Daniel Zrust <danielzrust@gmail.com>
2025-08-08 12:37:12 -0500

    feat: update the prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,12 @@
 module.exports = {
-  arrowParens: 'avoid',
-  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: false,
   trailingComma: 'all',
+  arrowParens: 'always',
+  semi: true,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  quoteProps: 'as-needed',
 };
